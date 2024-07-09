b0166b98ee26f3309be947c64a36c2e05ebb5212
sakshi <sakshiacc.13@gmail.com>
2024-07-09 22:30:23 +0530

    adding mul

--- mul.java
+++ mul.java
@@ -1,8 +1,8 @@
 public class mul {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 10;
         int b = 58;
         int c = a * b; // Result of multiplication
-        System.out.println("5 * 58 = " + c);
+        System.out.println("10 * 58 = " + c);
     }
 }
